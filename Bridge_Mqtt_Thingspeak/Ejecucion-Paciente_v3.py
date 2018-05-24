4002e74e74a6bb37df60fad2456f677d1cfd8cdb
jaimeVGR <36416323+jaimeVGR@users.noreply.github.com>
2018-05-24 14:53:21 +0200

    Version nueva
    Error al comparar con el txt para RFID

--- Bridge_Mqtt_Thingspeak/Ejecucion-Paciente_v3.py
+++ Bridge_Mqtt_Thingspeak/Ejecucion-Paciente_v3.py
@@ -15,20 +15,27 @@ def on_connect(client, userdata, flags, rc):
 
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
-	client.subscribe("Sistema/Wemos/Temperatura") # Asi se conecta a todos los mensa$
+	client.subscribe("Sistema/Wemos/#") # Asi se conecta a todos los mensajes
+	client.subscribe("Sistema/Movil/DNI")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
 	global flag_msg
 	global mensaje_mqtt 
+	global tema_mqtt
+
 	print("Recibido, Topic:"+msg.topic+" Mensaje:"+str(msg.payload))
+	tema_mqtt = str(msg.topic)
 	mensaje_mqtt = str(msg.payload)
+
 	flag_msg = 1
 
 
 def main(): 
 	global flag_msg
 	global mensaje_mqtt 
+	global tema_mqtt
+
 	client = mqtt.Client()
 	client.on_connect = on_connect
 	client.on_message = on_message
@@ -39,27 +46,53 @@ def main():
 	paciente = Paciente()
 
 	flag_msg = 0
-
+	flag_temp,flag_ox,flag_pulso = 0 , 0 ,0
+	pm, pv, tm, tv, om, ov = 0,0,0,0,0,0
 
 	while(1):
 
-		if flag_msg == 1:	#ERROR no consigo que tome esta rama...
-			(pm, pv, tm, tv, om, ov) = paciente.getDataFromSensor(mensaje_mqtt)
+		if flag_msg == 1:
 
-			if om < 95: 
-				print ("Envio mensaje de aviso por Telegram")
-				telegram = EnvioTelegram.TelegramBot()
-				telegram.getInformation()
-				telegram.send(paciente.getName(), "oxigeno")
+			if tema_mqtt.startswith("Sistema/Wemos/RFID"):
+				print ("RFID recibido")
+				if paciente.checkRFID(mensaje_mqtt):
+					client.publish("Sistema/Raspy",1)
+					print ("RFID correcto, envio respuesta")
+
+			elif tema_mqtt.startswith("Sistema/Movil"):
+				print ("Mensaje de movil")
+
+			elif tema_mqtt.startswith("Sistema/Wemos/Pulso"):
+				(tm,tv) = paciente.getDataFromSensor(mensaje_mqtt)
+
+				if (tm != 0):
+					flag_temp = 1
+
+			elif tema_mqtt.startswith("Sistema/Wemos/Oxigeno"):
+				(om, ov) = paciente.getDataFromSensor(mensaje_mqtt)
+
+				if (om != 0):
+					flag_ox = 1
+
+					if om < 95: 
+						print ("Envio mensaje de aviso por Telegram")
+						telegram = EnvioTelegram.TelegramBot()
+						telegram.getInformation()
+						telegram.send(paciente.getName(), "oxigeno")
+
+			elif tema_mqtt.startswith("Sistema/Wemos/Temperatura"):
+				(pm, pv) = paciente.getDataFromSensor(mensaje_mqtt)	
+				if (pm != 0 and pv != 0):
+					flag_temp = 1
 
 			else:
-				if tm == 0: # Es que no recibimos mensahe correcto
-					print("Mensaje incorrecto de mqtt")
-				else:
-					paciente.UpDataToThingspeak(int(pm), int(pv), int(tm), int(tv), int(om), int(ov))
+				print("Mensaje incorrecto de mqtt")
 
 			flag_msg = 0
 
+		if (flag_temp == 1) and (flag_ox == 1) and (flag_pulso == 1):
+			paciente.UpDataToThingspeak(pm, pv, tm, tv, om, ov)
+		
 		client.loop() # Se debe llamara regularmente para no desconectar de broker
 
 if __name__ == "__main__":
